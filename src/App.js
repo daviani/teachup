4794c22327b040d32c0d59a2690ee802493e5707
Daviani <contact@daviani.dev>
2021-12-13 08:55:12 +0100

    [FIX] Routing rules

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App () {
     return (
         <Router>
             <Switch>
-                    <Route path="/" component={Home}/>
+                    <Route exact path="/" component={Home}/>
                     <Route path='card' component={Card} />
                     <Route path="user" component={User}/>
                     <Route path="*" component={Error}/>
