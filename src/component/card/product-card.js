017fc7c4851cda15dea143a6714a9fa65e2e62fd
Daviani <contact@daviani.dev>
2021-12-14 14:13:26 +0100

    [DELETE] Unnecessary comment

--- src/component/card/product-card.js
+++ src/component/card/product-card.js
@@ -33,20 +33,3 @@ const path = ('product/' + plant.label)
 }
 
 export default ProductCard
-// <div style={{ width: '190px' }}>
-// <img src={plant.pictures[0]} alt={plant.plants[0].label} className="rounded-t-2xl"
-// style={{ height: '250px', width: '190px' }}/>
-// <div className="p-3 rounded-b-2xl"
-//      style={{ backgroundColor: '#f6ebf1' }}
-// >
-//     <div className="text-t-secondary">
-//         {label}
-//     </div>
-//     <div className="text-bold">
-//         {plant.label}
-//     </div>
-//     <div className="mt-3 text-primary">
-//         {/*IDR{' '}{plant.price}*/}
-//     </div>
-// </div>
-// </div>
