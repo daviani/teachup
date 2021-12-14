8f4b33f92f211e4a92f79667feff88801da952e6
Daviani <contact@daviani.dev>
2021-12-14 23:27:47 +0100

    [ADD] Install and configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+        'indent': ['error', 4],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'eqeqeq' : 'error',
+    }
+};
